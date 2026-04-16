6b1a34920cdf57ed6a8b9b3b71f5c855521f7287
李贵斌 <liguibin@athenase.vip>
2026-04-16 15:40:52 +0800

    Update HXNetworkLayer-Bridge-header.h
    git 教程

--- HXNetworkLayer/HXNetworkLayer-Bridge-header.h
+++ HXNetworkLayer/HXNetworkLayer-Bridge-header.h
@@ -11,3 +11,29 @@
 #import <Foundation/Foundation.h>
 #import "AddressContact.h"
 #import "LocalContactObjC.h"
+
+/**
+ 链接使用ssh最好
+
+ 1.创建空白github项目
+ 2.Pob lib create XXXX
+ 3.修改.podspec
+ 4.git add .
+ 5.Git commit -m ‘first cmmit’
+ 6.Git git remote add origin git@github.com:crazyLuobo/DeviceInformation.git
+ 7.git push -u origin main
+ 8 .git tag 0.1.0
+ 9.git push origin 0.1.0
+ 10.pod lib lint SystemBasicInfo.podspec --allow-warnings
+ 11.pod trunk register yanwenbo_78201@163.com “yanwenbo”
+ 12.pod trunk push /Users/account/Desktop/Repos/SystemBasicInfo/SystemBasicInfo.podspec --allow-warnings
+
+
+ git tag -d 0.1.0
+
+ git push origin --delete 0.1.0
+
+
+
+ pod trunk me
+ */
